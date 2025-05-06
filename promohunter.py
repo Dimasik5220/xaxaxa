360f902645a7f0851dc2528eea37571a44f82a6f
Dimasik5220 <Dmitrijsumin7220@gmail.com>
2025-05-06 21:38:00 +0300

    Добавление главного окна и кнопки перехода к игре
    Исправление ошибок

--- promohunter.py
+++ promohunter.py
@@ -12,18 +12,60 @@ from PyQt5.QtCore import Qt, QSize, QTimer
 from PyQt5.QtGui import QFont, QIcon, QColor
 
 
-class WelcomeScreen():
+class WelcomeWindow(QDialog):
     def __init__(self):
-        self.dialog = QDialog()
-        self.dialog.setTitle("Welcome") 
+        super().__init__()
+        self.setWindowTitle("Добро пожаловать")
+        self.setFixedSize(400, 200)
         layout = QVBoxLayout()
-        layout.add(QLabel("Name:"))  
-        layout.add(QPushButton("Start"))
-        self.dialog.setLayout(layout)
-        self.dialog.show()
+        self.name_label = QLabel("Введите ваше имя:")
+        self.name_input = QLineEdit()
+        self.lang_label = QLabel("Выберите язык:")
+        self.lang_combo = QComboBox()
+        self.lang_combo.addItems(["Русский", "English", "Беларуская", "Қазақша"])
+        self.start_btn = QPushButton("Начать")
+        self.start_btn.clicked.connect(self.accept)
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.name_input)
+        layout.addWidget(self.lang_label)
+        layout.addWidget(self.lang_combo)
+        layout.addWidget(self.start_btn)
+        self.setLayout(layout)
 
-class ConfigDialog():
-    def __init__(self):
+
+class SettingsDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Настройки")
+        self.setFixedSize(300, 200)
         layout = QVBoxLayout()
-        layout.add(QLabel("Theme:"))
-        layout.add(QPushButton("OK"))  
+        self.theme_label = QLabel("Выберите тему:")
+        self.theme_combo.addItems(["Синяя", "Темно-синяя", "Черная", "Серая", "Белая"])
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttons.accepted.connect(self.accept)
+        layout.addWidget(buttons)
+        self.setLayout(layout)
+
+class PromoHunter(QMainWindow):
+    def __init__(self, user_name="Пользователь", lang="Русский"):
+        self.setWindowTitle("PROMO HUNTER")
+        self.setFixedSize(1200, 800)
+
+        self.last_login = datetime.now().strftime("%Y-%m-%d %H:%M")
+        self.last_update = "Никогда"
+        self.current_theme = "Синяя"
+
+        if not os.path.exists("data"): os.makedirs("data")
+        if not os.path.exists("history"): os.makedirs("history")
+        if not os.path.exists("user_data"): os.makedirs("user_data")
+
+        self.favorites = self.load_json("user_data/favorites.json", [])
+        self.promocodes = []
+        self.cart = []
+
+        self.init_ui()
+        self.load_promocodes()
+        self.apply_theme(self.current_theme)
+
+        self.timer = QTimer(self)
+        self.timer.start(60000)
